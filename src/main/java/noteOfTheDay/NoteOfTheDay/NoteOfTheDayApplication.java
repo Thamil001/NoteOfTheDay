2c149f38f48788fcc1850cd315b78ec4f87b156c
Thamil001 <thamilthennarasu@gmail.com>
2025-12-04 12:01:14 +0530

    update pom.xml and Dockerfile

--- src/main/java/noteOfTheDay/NoteOfTheDay/NoteOfTheDayApplication.java
+++ src/main/java/noteOfTheDay/NoteOfTheDay/NoteOfTheDayApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NoteOfTheDayApplication {
+public class    NoteOfTheDayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NoteOfTheDayApplication.class, args);
